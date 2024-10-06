d36fabdcec078b01f588621ef2c2e0a1891915a3
amclubs <amclubs@github.com>
2024-10-06 15:33:48 +0800

    add am_restart_s5.sh

---
+++ am_restart_s5.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# 检查进程是否在运行
+pgrep -x "s5" > /dev/null
+
+# 如果没有运行，则启动 s5
+if [ $? -ne 0 ]; then
+    nohup /home/${USER}/.s5/s5 -c /home/${USER}/.s5/config.json >/dev/null 2>&1 &
+fi
